3244f8fc9213d495be04057693bdc5f78e32a7db
AliceTang <aliceblinmachinery@gmail.com>
2025-06-03 11:34:40 +0930

    update

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -7,6 +7,15 @@ import {motion,useInView} from "framer-motion"
 const projectData=[
   {
     id:1,
+    title:"AI-enhanced To-Do App",
+    description:"An AI-enhanced productivity app with task categorization, team collaboration, and planned PDF task extraction and priority recommendations, built with React, Next.js, .NET Core, Azure, and OpenAI API.",
+    image:"/images/projects/0.png",
+    tag:["All","Web"],
+    // gitUrl:"/",
+    previewUrl:"https://blotz-task-app.vercel.app/",
+  },
+  {
+    id:2,
     title:"Ecommerce Store",
     description:"A Next.js Ecommerce Store using TypeScript, Tailwindcss, headlessui, and Shopify CMS.",
     image:"/images/projects/1.png",
@@ -15,7 +24,7 @@ const projectData=[
     previewUrl:"https://next-commerce-alice.vercel.app/",
   },
   {
-    id:2,
+    id:3,
     title:"Veggify Recipe Blog App",
     description:"A veggify recipe blog app using MongoDB, Express.js, React.js and Node.js.",
     image:"/images/projects/2.png",
@@ -24,7 +33,7 @@ const projectData=[
     previewUrl:"https://veggify-fronted-app.vercel.app/",
   },
   {
-    id:3,
+    id:4,
     title:"Corporate Showcase Website",
     description:"A responsive agency website using Next.js 14, React, TypeScript, and Tailwind CSS.",
     image:"/images/projects/3.png",
@@ -33,7 +42,7 @@ const projectData=[
     previewUrl:"https://agency-yt-7v6c.vercel.app/",
   },
   {
-    id:4,
+    id:5,
     title:"Weather App",
     description:"A responsive weather website using HTML, CSS and JavaScript.",
     image:"/images/projects/4.png",
@@ -42,7 +51,7 @@ const projectData=[
     previewUrl:"https://dev-seven-wine.vercel.app/",
   },
   // {
-  //   id:5,
+  //   id:6,
   //   title:"Ecommerce Store",
   //   description:"Project 5 description",
   //   image:"/images/projects/5.png",
@@ -51,7 +60,7 @@ const projectData=[
   //   previewUrl:"https://next-commerce-alice.vercel.app/",
   // },
   // {
-  //   id:6,
+  //   id:7,
   //   title:"React Portfolio Website",
   //   description:"Project 6 description",
   //   image:"/images/projects/6.png",
